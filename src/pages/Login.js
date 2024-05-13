e526fa66550735b0a586743402cdbff0f85e55a9
neeraj negi <nnegi637673@gmail.com>
2024-05-13 21:42:14 +0530

    login as guest

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -47,6 +47,35 @@ export const Login = () => {
         }
   }
 
+  async function handleLoginAsGuest(){
+    email.current.value = process.env.REACT_APP_GUEST_LOGIN ;
+    password.current.value = process.env.REACT_APP_GUEST_PASSWORD ; 
+    try {
+      const authDetail = {
+        email: email.current.value,
+        password: password.current.value,
+      };
+      const data = await login(authDetail) ;
+      console.log(data);
+      data.accessToken ? navigate("/ProductList") : toast.error("email already exists", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        transition: Slide,
+      });
+  
+    } catch (error) {
+      toast.error(error.message , {closeButton:true , position:"bottom-center"})
+    }
+  
+  }
+
+
   return (
     <main>
       <section>
@@ -94,7 +123,7 @@ export const Login = () => {
           Log In
         </button>
       </form>
-      {/* <button className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button> */}
+      <button onClick={() => handleLoginAsGuest()} className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button>
     </main>
   );
 };
